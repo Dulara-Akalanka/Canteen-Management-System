ee27207cd9f445c0a6779d013ce32c56fbf8dd2b
Dulara Akalanka <95605831+Dulara-Akalanka@users.noreply.github.com>
2022-11-16 05:17:02 +0530

    Update code.php

--- admin/supplier/code.php
+++ admin/supplier/code.php
@@ -1,32 +1,30 @@
 <?php
 session_start();
-require 'dbcon.php';
+require 'conn.php';
 
 if(isset($_POST['SUBMIT']))
 {
- $item_id = mysqli_real_escape_string($conn, $_POST['ID']);
- $item_name = mysqli_real_escape_string($conn, $_POST['name']);
- $brand = mysqli_real_escape_string($conn, $_POST['brand']);
- $quantity = mysqli_real_escape_string($conn, $_POST['quantity']);
- $unit_price = mysqli_real_escape_string($conn, $_POST['price']);
- $d_o_m = mysqli_real_escape_string($conn, $_POST['date-m']);
- $d_o_e = mysqli_real_escape_string($conn, $_POST['date-e']);
- $emp_id = mysqli_real_escape_string($conn, $_POST['employee_id']);
- $supplier_id = mysqli_real_escape_string($conn, $_POST['supplier_id']);
+ $sup_ID = mysqli_real_escape_string($conn, $_POST['ID']);
+ $sup_name = mysqli_real_escape_string($conn, $_POST['name']);
+ $sup_address = mysqli_real_escape_string($conn, $_POST['address']);
+ $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+ $date = mysqli_real_escape_string($conn, $_POST['date']);
+ $payment = mysqli_real_escape_string($conn, $_POST['payment']);
+ 
 
-$query ="INSERT INTO item (item_id,item_name,brand,quantity,unit_price,d_o_m,d_o_e,emp_id,supplier_id	)
- VALUES ('$item_id','$item_name','$brand','$quantity','$unit_price','$d_o_m','$d_o_e','$emp_id','$supplier_id')";
+$query ="INSERT INTO supplier (sup_id,sup_name,sup_address,sup_phone_no,supply_date,sup_payment)
+ VALUES ('$sup_ID','$sup_name','$sup_address','$phone','$date','$payment')";
 
 $query_run = mysqli_query($conn,$query);
 if($query_run)
 {
-    $_SESSION['message']="ITEM INSERTED SUCCESSFULLY";
- header("Location: item.php");
+    $_SESSION['message']="SUPPLIER INSERTED SUCCESSFULLY";
+ header("Location: supplier.php");
  exit(0);
 }
 else{
-    $_SESSION['message']="ITEM NOT INSERTED SUCCESSFULLY";
- header("Location: item.php");
+    $_SESSION['message']="SUPPLIER NOT INSERTED SUCCESSFULLY";
+ header("Location: supplier.php");
  exit(0);
 }
 }
