4b9fd7e9059eaa5172f29b1b27d29dce71cb7006
Dulara Akalanka <95605831+Dulara-Akalanka@users.noreply.github.com>
2022-11-16 05:18:48 +0530

    Update supplier.php

--- admin/supplier/supplier.php
+++ admin/supplier/supplier.php
@@ -6,7 +6,7 @@ session_start();
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ITEM INSERT</title>
+    <title>ADD SUPPLIER</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   </head>
   <body>
@@ -21,7 +21,7 @@ session_start();
     <div class="col-md-12">
     <div class="card">
     <div class="card-header">
-        <h4> ITEM ADD
+        <h4> ADD SUPPLIER
         <a href="index.php" class="btn btn-danger float-end">BACK</a>
 </h4>
 </div>
@@ -37,24 +37,20 @@ session_start();
         <input type="text" name="name" class="form-control">
         </div>
         <div class="mb-3">
-        <label>Supplier_Name</label>
-        <input type="text" name="brand" class="form-control">
+        <label>Supplier_Address</label>
+        <input type="text" name="address" class="form-control">
         </div>
         <div class="mb-3">
         <label>Supplier_Phone_No</label>
-        <input type="text" name="quantity" class="form-control">
+        <input type="text" name="phone" class="form-control">
         </div>
         <div class="mb-3">
         <label>Supply_Date</label>
-        <input type="text" name="price" class="form-control">
+        <input type="date" name="date" class="form-control">
         </div>
         <div class="mb-3">
-        <label>EMPLOYEE_ID</label>
-        <input type="text" name="employee_id" class="form-control">
-        </div>
-        <div class="mb-3">
-        <label>SUPPLIER_ID</label>
-        <input type="text" name="supplier_id" class="form-control">
+        <label>SUPPLIER_PAYMENT</label>
+        <input type="text" name="payment" class="form-control">
         </div>
 
         <div class="mb-3">
